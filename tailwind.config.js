f6d5c7bb6d42cc411996f0fdf0eb8ed2fdb7bc5d
eMist <edrylmoratas.mist.com>
2024-05-07 02:16:20 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'primary': "#0D375E",
+        'secondary': "#FF9114",
+      }
+    },
+  },
+  plugins: [],
+}
+
